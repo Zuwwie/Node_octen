d797fafec516e0f5f40caf788a137965e6d5e1b9
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-09-30 18:24:09 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//  - у вас є масив юзрів (до 10), з такими полями наприклад - const users = [
+//     { name: 'olya', gender: 'female', age: 20 }
+//         ...
+// ], вам потрібно написати метод який створює файлики - де назва файлику - це імя вашого юзера (наприклад - Olya.txt),
+// вміст це сам ваш юзер - { name: 'olya', gender: 'female', age: 20 }
+// перед тим створити 4 папки (програмно) - наприклад - manOlder20, manYounger20, womanOlder20, womanYounger20
+// і розподілити ваших юзерів саме по відповідних папках
+
+const users = [
+    { name: 'olya', gender: 'female', age: 20 },
+    { name: 'dima', gender: 'male', age: 22 },
+    { name: 'anna', gender: 'female', age: 19 },
+    { name: 'victor', gender: 'male', age: 99 },
+    { name: 'kate', gender: 'female', age: 22 },
+    { name: 'nazar', gender: 'male', age: 77 },
+    { name: 'vasul', gender: 'male', age: 16 },
+    { name: 'MS_Z', gender: 'female', age: 20 }
+]
+
